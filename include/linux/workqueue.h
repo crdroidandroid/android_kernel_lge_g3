2efff0ea12d898bfaea28c2ccae68134d7357639
Tejun Heo <tj@kernel.org>
2013-03-12 11:30:03 -0700

    workqueue: drop WQ_RESCUER and test workqueue->rescuer for NULL instead
    WQ_RESCUER is superflous.  WQ_MEM_RECLAIM indicates that the user
    wants a rescuer and testing wq->rescuer for NULL can answer whether a
    given workqueue has a rescuer or not.  Drop WQ_RESCUER and test
    wq->rescuer directly.
    
    This will help simplifying __alloc_workqueue_key() failure path by
    allowing it to use destroy_workqueue() on a partially constructed
    workqueue, which in turn will help implementing dynamic management of
    pool_workqueues.
    
    While at it, clear wq->rescuer after freeing it in
    destroy_workqueue().  This is a precaution as scheduled changes will
    make destruction more complex.
    
    This patch doesn't introduce any functional changes.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Lai Jiangshan <laijs@cn.fujitsu.com>

--- include/linux/workqueue.h
+++ include/linux/workqueue.h
@@ -321,8 +321,7 @@ enum {
 	 */
 	WQ_POWER_EFFICIENT	= 1 << 7,
 
-	WQ_DRAINING		= 1 << 16, /* internal: workqueue is draining */
-	WQ_RESCUER		= 1 << 17, /* internal: workqueue has rescuer */
+	WQ_DRAINING		= 1 << 6, /* internal: workqueue is draining */
 
 	WQ_MAX_ACTIVE		= 512,	  /* I like 512, better ideas? */
 	WQ_MAX_UNBOUND_PER_CPU	= 4,	  /* 4 * #cpus for unbound wq */
