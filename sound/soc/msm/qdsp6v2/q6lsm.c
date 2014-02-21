83a3db19f4823d90634840f59f73a9ff5dde3aa3
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2014-02-20 18:52:12 -0800

    ASoC: msm: qdsp6v2: Cleanup of audio drivers
    Using the format strings will remove need for typecasting.
    This change cleans up audio msm drivers to use the kernel
    defined data types and format specifiers for printk.
    
    Change-Id: Ie1b3d5c8dcf868aa1cc7407d3e679976dffafc76
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6lsm.c
+++ sound/soc/msm/qdsp6v2/q6lsm.c
@@ -29,8 +29,8 @@
 #include <sound/q6core.h>
 #include <sound/q6lsm.h>
 #include <asm/ioctls.h>
-#include <mach/memory.h>
-#include <mach/debug_mm.h>
+#include <linux/memory.h>
+#include <linux/msm_audio_ion.h>
 #include "audio_acdb.h"
 
 #define APR_TIMEOUT	(5 * HZ)
@@ -58,7 +58,6 @@ struct lsm_common {
 };
 
 static struct lsm_common lsm_common;
-
 /*
  * mmap_handle_p can point either client->sound_model.mem_map_handle or
  * lsm_common.mmap_handle_for_cal.
@@ -729,7 +728,7 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 	memset(&lsm_cal, 0, sizeof(lsm_cal));
 	mutex_lock(&client->cmd_lock);
 	get_lsm_cal(&lsm_cal);
-	pr_debug("%s:Snd Model len = %d cal size %d", __func__,
+	pr_debug("%s:Snd Model len = %zd cal size %zd", __func__,
 			 len, lsm_cal.cal_size);
 	if (!lsm_cal.cal_paddr) {
 		pr_err("%s: No LSM calibration set for session", __func__);
@@ -750,7 +749,7 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 			goto fail;
 		}
 		total_mem = pad_zero + len + lsm_cal.cal_size;
-		pr_debug("%s: Pad zeros sound model %d Total mem %d\n",
+		pr_debug("%s: Pad zeros sound model %zd Total mem %zd\n",
 				 __func__, pad_zero, total_mem);
 		client->sound_model.client =
 		    msm_ion_client_create(UINT_MAX, "lsm_client");
