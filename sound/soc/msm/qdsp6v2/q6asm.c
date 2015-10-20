fa4baf9fe2b4d24e60825e5cd4c06cb6858bc396
Ben Romberger <bromberg@codeaurora.org>
2015-10-20 14:13:32 -0700

    ASoC: msm: qdsp6v2: Initialize ASM no wait spinlock
    Initialize spinlock for the no wait queue. The
    spin_lock_init function is now called for the
    spinlock on init for the audio client structure.
    
    Change-Id: I14a6e9d5364a48e8d43c798067356524020cc007
    Signed-off-by: Ben Romberger <bromberg@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -930,6 +930,8 @@ struct audio_client *q6asm_audio_client_alloc(app_cb cb, void *priv)
 	}
 	atomic_set(&ac->cmd_state, 0);
 	atomic_set(&ac->nowait_cmd_cnt, 0);
+	spin_lock_init(&ac->no_wait_que_spinlock);
+	INIT_LIST_HEAD(&ac->no_wait_que);
 	atomic_set(&ac->mem_state, 0);
 
 	send_asm_custom_topology(ac);
@@ -4867,6 +4869,8 @@ static int __init q6asm_init(void)
 	}
 	atomic_set(&common_client.cmd_state, 0);
 	atomic_set(&common_client.nowait_cmd_cnt, 0);
+	spin_lock_init(&common_client.no_wait_que_spinlock);
+	INIT_LIST_HEAD(&common_client.no_wait_que);
 	atomic_set(&common_client.mem_state, 0);
 
 	config_debug_fs_init();
