45f358521636e4bf49a2c8b52572c93eb718ad1d
Dorimanx <yuri@bynet.co.il>
2015-10-27 13:39:19 +0200

    drivers: state_notifier: cleanups and add delay to state_suspended.
    -I use the state_suspended only for cortex script in userspace
    so i want first to wake all in kernel, then allow cortex to do it's work.
    i seen crash kernel BUG at fs/dcache.c:2157! right after wakeup
    as cortex was changing cpu freqs before or at same time with state notifier!
    added small delay before telling cortex to wakeup.
    -Cleaned not used FB callback.
    -set 444 perm for state_suspended and made it static.
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1215,8 +1215,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
 		scr_suspended = false;
 #endif
-		if (!use_fb_notifier)
-			state_resume();
+		state_resume();
 #endif
 		break;
 	case MDSS_EVENT_BLANK:
@@ -1232,8 +1231,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
 		scr_suspended = true;
 #endif
-		if (!use_fb_notifier)
-			state_suspend();
+		state_suspend();
 #endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
