34569d8783f80cbe1cad91322a85498847f92488
dadi11 <dadi.sikorski@gmail.com>
2016-03-24 10:40:05 +0100

    Revert "Enable powersuspend"
    This reverts commit 19cbcf62aa6f981876ebfc9fd3c6d97601f77dfb.
    
    Revert "PowerSuspend: V1.7.1 Re Added AutoSleep and Hybrid LCD Hooks back"
    
    This reverts commit a70cc1c8f450d3a409759d385910106d1f39b842.
    
    Revert "kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.7 (faux123/Yank555.lu)"
    
    This reverts commit a50951214e71646d7291c53a4ab173ec1e9a3362.
    
    Revert "kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.7 (faux123/Yank555.lu)"
    
    This reverts commit da02251e189f806d3eaa24e63e5d1f6650065290.
    
    Revert "msm_adreno_tz: update Adreno idler"
    
    This reverts commit f9949542a11129efeba1041c3edc58508b05d539.
    
    Revert "kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.6 (faux123/Yank555.lu)"
    
    This reverts commit 123d540a9b8439e45cae90c27033ae414eb74ed0.
    
    Revert "power: make sync on suspend optional"
    
    This reverts commit dd805edd6d262bc909de19cf04b631298e274bce.
    
    Revert "msm_adreno_tz: fix-up tz_suspend"
    
    This reverts commit 2a82f72de313eb5fff21a1090e893598e540639a.
    
    Revert "msm_adreno_tz: be aware of suspended state"
    
    This reverts commit e40bb180f6a1b6cdda317418b5008ca99c1eebfa.
    
    Revert "powersuspend: add missing mdss_dsi_panel.c hooks"
    
    This reverts commit 052e955ba7f890425a628d785af91088aee56e6d.
    
    Revert "powersuspend: add power_suspended boolean for global access"
    
    This reverts commit 0ef31e4a5d247fe218c7caf5d8f938afc31b0547.
    
    Revert "kernel/power/powersuspend: cumulative update to version 1.5"
    
    This reverts commit 7a49201feab3fd1a6b9efb4b064d8b4bf2d5cb20.
    
    Revert "powersuspend: active from panel, inactive from autosleep"
    
    This reverts commit 9bacb884b851dda13dd07a6665c291d65dbf0889.
    
    Revert "powersuspend: default to display hooks"
    
    This reverts commit 17565886d9ecd6220e9b5a5c065a1b5195b8ae70.
    
    Revert "powersuspend: minor typo fix"
    
    This reverts commit c01334471cbe3393b73237afa35c6af450dd38e9.
    
    Revert "powersuspend: add LCD screen on/off hooks (Yank555.lu)"
    
    This reverts commit f09df43c9bc14ef7bce3e990d66854b7c3e8408f.
    
    Revert "kernel/power/powersuspend: add back userpace control w/ default kernel control"
    
    This reverts commit cf65e7f2b7a8baa1699fb4f0fbf6060b8a67aa85.
    
    Revert "kernel/power/powersuspend: remove userspace dependency from powersuspend"
    
    This reverts commit a28bd5330fe2ae3819a7abe20e41ae0a2dc7d36a.
    
    Revert "powersuspend: fix logci derps :p"
    
    This reverts commit 63f19376c311acdff893a31c88b00c87ab473c7f.
    
    Revert "kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend"
    
    This reverts commit a9c4ca63de88979fd2a2ef48643fb7bc1fae8868.
    
    Conflicts:
    	drivers/devfreq/governor_msm_adreno_tz.c
    
    Change-Id: Ieac446e315075d99d7b3255e7afd7c9e8e4ceacb

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -19,7 +19,6 @@
 #include <linux/io.h>
 #include <linux/ftrace.h>
 #include <linux/msm_adreno_devfreq.h>
-#include <linux/powersuspend.h>
 #include <mach/scm.h>
 #include "governor.h"
 
@@ -45,9 +44,6 @@ static DEFINE_SPINLOCK(tz_lock);
 #define TZ_INIT_ID		0x6
 
 #define TAG "msm_adreno_tz: "
-
-/* Boolean to detect if panel has gone off */
-static bool power_suspended = false;
 
 /* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry2(u32 cmd, u32 val1, u32 val2)
@@ -106,18 +102,14 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		return result;
 	}
 
-	*freq = stats.current_frequency;
-	*flag = 0;
-
-	/*
-	 * Force to use & record as min freq when system has
-	 * entered pm-suspend or screen-off state.
-	 */
-	if (power_suspended) {
-		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
-		return 0;
+	/* Prevent overflow */
+	if (stats.busy_time >= (1 << 24) || stats.total_time >= (1 << 24)) {
+		stats.busy_time >>= 7;
+		stats.total_time >>= 7;
 	}
 
+	*freq = stats.current_frequency;
+	*flag = 0;
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
 	if (priv->bus.num) {
@@ -354,26 +346,8 @@ static struct devfreq_governor msm_adreno_tz = {
 	.event_handler = tz_handler,
 };
 
-static void tz_early_suspend(struct power_suspend *handler)
-{
-	power_suspended = true;
-	return;
-}
-
-static void tz_late_resume(struct power_suspend *handler)
-{
-	power_suspended = false;
-	return;
-}
-
-static struct power_suspend tz_power_suspend = {
-	.suspend = tz_early_suspend,
-	.resume = tz_late_resume,
-};
-
 static int __init msm_adreno_tz_init(void)
 {
-	register_power_suspend(&tz_power_suspend);
 	return devfreq_add_governor(&msm_adreno_tz);
 }
 subsys_initcall(msm_adreno_tz_init);
