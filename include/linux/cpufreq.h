9555a6c7cdf5bd6129be6e3181ea6c5ef96a88a1
ppajda <pajdek@gmail.com>
2017-10-18 15:08:55 +0200

    Add blu active gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -413,6 +413,9 @@ extern struct cpufreq_governor cpufreq_gov_slim;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 
