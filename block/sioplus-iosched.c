23ef818b0c3b7da66809e52deb6b16faef0ae6f5
ppajda <pajdek@gmail.com>
2017-11-23 15:05:18 +0100

    block: set fifo batch to 16 to reduce overload on eMMC and CPU

--- block/sioplus-iosched.c
+++ block/sioplus-iosched.c
@@ -32,7 +32,7 @@ static const int sync_write_expire = (HZ / 4) * 5;	/* max time before a sync wri
 static const int async_read_expire = (HZ / 2);	/* ditto for async, these limits are SOFT! */
 static const int async_write_expire = (HZ * 2);	/* ditto for async, these limits are SOFT! */
 
-static const int writes_starved = 1;		/* max times reads can starve a write */
+static const int writes_starved = 2;		/* max times reads can starve a write */
 static const int fifo_batch     = 16;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
